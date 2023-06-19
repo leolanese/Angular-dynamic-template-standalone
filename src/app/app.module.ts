7db036d4c2b171dc293d6685e5d9f8e274bd8cfd
leolanese <laneseleo@gmail.com>
2023-06-19 10:11:10 +0100

    hasError + bootstrapApplication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,6 @@ import { AppComponent } from './app.component';
 })
 export class AppModule { }
 
-
 // The AppComponent class is a standalone component. This means that it does not belong to any module.
 // The @NgModule.bootstrap array can only contain components that belong to a module.
 // The bootstrapApplication function can be used to bootstrap any component, including standalone components.
